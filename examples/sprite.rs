e59b6d73d117dbf03609cc6d30d62e97d3a18c30
Tim Jentzsch <commits@timjen.net>
2023-02-24 16:43:30 +0100

    Add example for loading a Krita document as a sprite

---
+++ examples/sprite.rs
@@ -0,0 +1,21 @@
+//! Displays a single [`Sprite`], created from a Krita document.
+
+use bevy::prelude::*;
+use bevy_mod_krita::KritaPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        // Add the Krita plugin to enable loading of `.kra` files
+        .add_plugin(KritaPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("krita/demo.kra"),
+        ..default()
+    });
+}
